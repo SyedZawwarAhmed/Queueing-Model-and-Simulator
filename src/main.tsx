28c21db3eab819c6beefd961790508de9dac739f
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-13 21:30:57 +0500

    delete 2 files, update 1 file and move 4 files

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Priority from "./pages/Priority/index.tsx";
-import Simulator from "./pages/Simulator/index.tsx";
-import QueueingModel from "./pages/QueueingModel/index.tsx";
+import Priority from "./routes/Priority/index.tsx";
+import Simulator from "./routes/Simulator/index.tsx";
+import QueueingModel from "./routes/QueueingModel/index.tsx";
 import App from "./App.tsx";
 
 const router = createBrowserRouter([
