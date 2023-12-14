0644507252ddb1362f45829e8bb6e23f42c7485d
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-12-14 09:40:32 +0500

    update index.html and src/components/Input/index.tsx

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 function Input({ label, setValue }) {
+  const preventMinus = (e) => {
+    if (e.code === 'Minus') {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div style={{ margin: "1rem" }}>
       <h2>{label}</h2>
@@ -8,7 +14,11 @@ function Input({ label, setValue }) {
         style={{ fontSize: "2rem", padding: "1rem" }}
         type="number"
         min={0}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => {
+          const inputValue = Math.max(0, Number(e.target.value));
+          setValue(inputValue);
+        }}
+        onKeyDown={preventMinus}
       />
     </div>
   );
